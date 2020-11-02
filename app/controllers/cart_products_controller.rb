105c909d40a601aacb9b3ce4f66a810ac59a4070
Takanari Nishio <taka0824nari@gmail.com>
2020-11-02 02:14:11 +0000

    cart_products#index_added

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,14 +1,39 @@
 class CartProductsController < ApplicationController
-  def index
+  
+  def create
+    @cart_product = CartItem.new(cart_item_params)
+    @cart_product.customer_id = current_customer.id
+    if current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id]).present?
+    # product_id: params[:cart_product][:product_id])[]が2つ続く記述はどんな意味？product_idを指定するのになぜ2つパラメーターが必要？
+    #cart_productsテーブルにすでに同じproductがあった時に、個数をプラスさせるための記述
+    #見本ではsaveになっているがupdateじゃないの？
+    elsif @cart_product.save
+      flash[:notice] = "New Product was successfully added to cart."
+      # redirect_to show画面
+    else
+      # render 'show画面'
+    end
   end
-
-  def new
+  
+  def index
+    @cart_products = CartProduct.all
+  end
+  
+  def update
   end
 
   def show
   end
 
-  def edit
+  def destroy
   end
-
+  
+  def destroy_all
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:product_id, :quantity)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'homes#top'
-  get 'homes/about' => 'homes#about'
+    root 'homes#top'
+    get 'homes/about' => 'homes#about'
 
-  devise_for :admins
-  devise_for :customers do
+    devise_for :admins
+    devise_for :customers
 
-  delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
-  get 'customers/resign' => 'custmers#resign'
-  get 'orders/thanks' => 'orders#thanks'
+    delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
+    get 'customers/resign' => 'custmers#resign'
+    get 'orders/thanks' => 'orders#thanks'
 
-  namespace :admins do
-    resources :orders
-    resource :customers
-    resources :products
-    resources :order_products do
-      resources :genres
+    namespace :admins do
+      resources :orders
+      resource :customers
+      resources :products
+      resources :order_products do
+        resources :genres
+      end
+      get 'orders/number' => 'orders#number'
     end
-    get 'orders/number' => 'orders#number'
-  end
-
-
- resource :custmers do
-   resources :address
- end
- resources :orders
- resources :products
- resources :order_products
- resources :cart_products
-
-end
+    
+   resource :custmers do
+     resources :address
+   end
+   resources :orders
+   resources :products
+   resources :order_products
+   resources :cart_products
 end
