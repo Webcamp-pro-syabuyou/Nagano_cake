9c2908b80c8f3040b5cf7d678bdc5c478af97038
Takanari Nishio <taka0824nari@gmail.com>
2020-11-03 06:24:59 +0000

    route_updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-   get "cart_products/put_in_cart" => "cart_products#put_in_cart"
-  # push前に削除
-
     root 'homes#top'
     get 'homes/about' => 'homes#about'
-
     devise_for :customers
     devise_for :admins
-
-
     delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
     get 'customers/resign' => 'custmers#resign'
     get 'orders/thanks' => 'orders#thanks'
