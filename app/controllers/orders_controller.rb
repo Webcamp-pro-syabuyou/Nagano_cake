4f8ae5aeead8c32e9b6f833e30c25a9b3ef17aab
EC2 Default User <ec2-user@ip-172-31-53-160.ec2.internal>
2020-11-08 09:24:42 +0000

    [Add]Order関連

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,17 @@ class OrdersController < ApplicationController
   before_action :confirm_params, only: [:confirm]
 
   def index
+    @orders = current_customer.orders.all
+   array = []
+     current_customer.cart_products.all.each do |cart_product|
+      array << cart_product.product.price * cart_product.quantity
+     end
+     @total_price = (array.sum * 1.1).floor
+     @postage = 800
+
+
+  # @cart_product = current_customer.cart_product
+
   end
 
   def new
@@ -12,15 +23,20 @@ class OrdersController < ApplicationController
 
 
   def confirm
-
-   # @test = params[:order]
+     @postage = 800
+     @cart_products = current_customer.cart_products
+     array = []
+     current_customer.cart_products.all.each do |cart_product|
+      array << cart_product.product.price * cart_product.quantity
+     end
+     @total_price = (array.sum * 1.1).floor
 
      return if @order.valid?
        @payment_method = params[:order][:payment_method]
       if params[:order][:address_option] == "0"
         @order.postalcode = current_customer.postalcode
         @order.delivery_address = current_customer.address
-        @order.delivery_name = current_customer.family_name
+        @order.delivery_name = current_customer.family_name + " " + current_customer.first_name
 
       elsif params[:order][:address_option] == "1"
         address = Address.find(params[:order][:id])
@@ -29,12 +45,6 @@ class OrdersController < ApplicationController
         @order.delivery_name = address.delivery_name
       end
       render 'confirm'
-
-      # else
-      #   @order.postalcode = params[:order][:postalcode]
-      #   @order.delivery_address = params[:order][:new_address]
-      #   @order.delivery_name = params[:order][:delivery_name]
-
   end
 
   def thanks
@@ -42,12 +52,32 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
-    @order.save
+    @order = Order.new(
+      postalcode: params[:order][:postalcode],
+      customer_id: current_customer.id,
+      delivery_address: params[:order][:delivery_address],
+      delivery_name: params[:order][:delivery_name],
+      payment_method: params[:order][:payment_method],
+      postage: params[:order][:postage],
+      order_status: params[:order][:order_status])
+
+    if @order.save
     redirect_to orders_thanks_path
+    else
+     render 'confirm'
+    end
   end
 
   def show
+    @order = Order.find(params[:id])
+    @postage = 800
+
+    @cart_products = current_customer.cart_products
+     array = []
+     current_customer.cart_products.all.each do |cart_product|
+      array << cart_product.product.price * cart_product.quantity
+     end
+    @total_price = (array.sum * 1.1).floor
   end
 
   def edit
@@ -56,11 +86,11 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:customer_id, :postalcode, :delivery_address, :delivery_name, :postage, :total_price, :payment_method, :order_status)
+    params.require(:order).permit(:customer_id, :postalcode, :delivery_address, :delivery_name, :payment_method, :total_price, :order_status, :postage)
 
   end
 
   def confirm_params
-    @order = Order.new(params.require(:order).permit(:postalcode, :delivery_address, :delivery_name, :payment_method))
+    @order = Order.new(params.require(:order).permit(:customer_id, :postalcode, :delivery_address, :delivery_name, :payment_method, :total_price, :order_status, :postage))
   end
 end
