d34fd48ca02e3b8865a4d74bd80e5b3cdfb48b74
EC2 Default User <ec2-user@ip-172-31-53-160.ec2.internal>
2020-11-10 07:52:28 +0000

    [Add]レイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,20 @@
 class HomesController < ApplicationController 
   def top
+     @genres = Genre.where(is_active: true)
+     	# もしURLに[:genre_id]が含まれていたら
+    	if params[:genre_id]
+    		# その[:genre_id]のデータをGenreから@genreに入れて
+    		@genre = Genre.find(params[:genre_id])
+  	    # @genreに紐付いた商品で販売可の商品を持ってくるイメージ。全件抽出する（あとでカウントしたいので）
+
+    		@products = @genre.products.order(created_at: :desc).where(is_active: true).page(params[:page])
+    		#binding.pry
+        else
+        # whereメソッドを使うときは booleanの値を整数に。全件抽出する（あとでカウントしたいので）
+        
+        @products = Product.where(is_active: true).page(params[:page])
+        
+        end
   end
   
   def about
