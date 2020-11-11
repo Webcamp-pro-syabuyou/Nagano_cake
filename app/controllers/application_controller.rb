873bbfbc24a501e4d3063af88164c97db68d7b12
asanuma <moeko.a980@gmail.com>
2020-11-11 08:05:04 +0000

    [change]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_out_path_for(resource)
-    case resource
-      when Admin
-        new_admin_session_path
-      when  Customer
-        root_path
-    end
-  end
+  # def after_sign_out_path_for(resource)
+  #   case resource
+  #     when Admin
+  #       new_admin_session_path
+  #     when  Customer
+  #       root_path
+  #   end
+  # end
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postalcode, :address, :tel])
